0a1d85db3920f4e3a85ba7f4bc6602807060105d
samyam <samyamshr@gmail.com>
2023-09-17 10:14:41 +0545

    This is a flutter app

---
+++ lib/AllScreens/login_screen.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView( // Add SingleChildScrollView here
+        child: Column(
+          children: [
+            SizedBox(
+              height: 0.0,
+            ),
+            Image(
+              image: AssetImage("images/logo.png"),
+              width: 350.0,
+              height: 350.0,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 1.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Usename",
+                      labelStyle: TextStyle(fontSize: 14.0),
+                      hintStyle: TextStyle(
+                        color: Colors.purpleAccent,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 1.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(fontSize: 14.0),
+                      hintStyle: TextStyle(
+                        color: Colors.purpleAccent,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFAD88BA)),
+                    ),
+                    child: Container(
+                      height: 50.0,
+                      width: 50.0,
+                      child: Center(
+                        child: Text("Login"),
+                      ),
+                    ),
+                    onPressed: () {
+                      print("hello");
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFAD88BA)),
+                    ),
+              onPressed: () {},
+              child: Text("Dont have an account? Register Here"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:team_rescue_routes/AllScreens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rescue routes',
+      theme: ThemeData(
+        fontFamily: "Poppins",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
