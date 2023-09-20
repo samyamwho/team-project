3e83f1e615a70ec82bf39c5abad7935092ddbf59
Nischal Basnet <nischallukla@gmail.com>
2023-09-20 12:48:01 +0545

    commit 1

---
+++ team-project/android/app/src/main/kotlin/com/example/team_rescue_routes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.team_rescue_routes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ team-project/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:team_rescue_routes/AllScreens/login_screen.dart';
+import 'package:team_rescue_routes/AllScreens/mainscreen.dart';
+import 'package:team_rescue_routes/AllScreens/registration_Screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+DatabaseReference usersRef = FirebaseDatabase.instance.ref().child("users");
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rescue routes',
+      theme: ThemeData(
+        fontFamily: "Poppins",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: MainScreen.idScreen,
+      routes: {
+        RegistrationScreen.idScreen: (context) => RegistrationScreen(),
+        LoginScreen.idScreen: (context) => LoginScreen(),
+        MainScreen.idScreen: (context) => const MainScreen(),
+      },
+    );
+  }
+}
+
+// kya gunda banega re tu
+// hello world
